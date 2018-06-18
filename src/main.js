21cf94de227cbe44cfcac66821ec4cf72ed7b013
xwq863728739 <email863728739@qq.com>
2018-06-18 21:12:08 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// 找到的是App.vue文件
 import App from './App'
 import router from './router'
+import './assets/styles/reset.css'
+// 解决1像素边框问题
+import './assets/styles/border.css'
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  // es6的简写{App:App}
   components: { App },
   template: '<App/>'
 })
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div> 
+        <div>home</div>
+        <router-link to='/list'>列表页</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Home'
+}
+</script>
+
+<style>
+    
+</style>
